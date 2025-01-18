80d1c7270ea030989b5f3ab09ef270083cf55505
RollingMushroom <aaronsimple31@gmail.com>
2025-01-19 04:32:00 +0800

    Admin can add book and user

--- EnterpriseApplication4-war/build/generated-sources/ap-source-output/entities/Books_.java
+++ EnterpriseApplication4-war/build/generated-sources/ap-source-output/entities/Books_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2025-01-19T03:53:29")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2025-01-19T04:28:31")
 @StaticMetamodel(Books.class)
 public class Books_ { 
 
