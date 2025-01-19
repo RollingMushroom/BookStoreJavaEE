cc259f3ab1129eb413c99f0fb7bac7172c71fdb6
RollingMushroom <aaronsimple31@gmail.com>
2025-01-19 15:13:43 +0800

    Admin Page

--- EnterpriseApplication4-war/build/web/WEB-INF/classes/sql/add_description_column.sql
+++
@@ -1,5 +0,0 @@
--- Add description column to BOOKS table
-ALTER TABLE BOOKS ADD DESCRIPTION VARCHAR(2000);
-
--- Update existing books with sample descriptions (optional)
-UPDATE BOOKS SET DESCRIPTION = 'No description available.' WHERE DESCRIPTION IS NULL; 
